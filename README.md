b74d5375604899cda28bf514cfaab73e8ae01edb
Theodorus Clarence <theodorusclarence@gmail.com>
2021-03-23 23:31:26 +0700

    finished 2b, 2c

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Soal Shift 1
 [Soal](https://docs.google.com/document/d/1T3Y4o2lt5JvLTHdgzA5vRBQ0QYempbC5z-jcDAjela0/edit)
 
-## Referensi
+## Referensi Nomor 2
 - cek cara split tab, nomor 2 https://stackoverflow.com/questions/44394034/how-to-view-syslog-in-ubuntu
+- cari max, https://www.programmersought.com/article/4079936922/
+- cari substring buat tanggal, https://community.hpe.com/t5/Languages-and-Scripting/print-the-last-character-of-a-string-using-awk/td-p/5162994#.YFoTwmgxXOR
+- cari muncul berapa kali https://stackoverflow.com/questions/27986425/using-awk-to-count-the-number-of-occurrences-of-a-word-in-a-column
--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -3,7 +3,7 @@
 # cara separate tabs
 # awk -F$'\t' '{print $1}'
 
-
+# Row ID dan profit percentage terbesar (jika hasil profit percentage terbesar lebih dari 1, maka ambil Row ID yang paling besar)
 awk -F"\t" '
 BEGIN {max=0;rid=0;}
 {sales=$18;profit=$21;rid=$2;
@@ -19,4 +19,36 @@ BEGIN {max=0;rid=0;}
 }
 END {
     printf("Transaksi terakhir dengan profit percentage terbesar yaitu %s dengan persentase %s.\n", maxidx,max)
+}' Laporan-TokoShiSop.tsv
+
+echo
+
+# nama customer pada transaksi tahun 2017 di Albuquerque.
+awk -F"\t" '
+BEGIN {print "Daftar nama customer di Albuquerque pada tahun 2017 antara lain:"}
+{city=$10;trx=$3;name=$7
+    last2trxdate=substr(trx,length(trx)-1,2)
+    if (city == "Albuquerque" && last2trxdate == "17")
+        print name
+}
+END {}' Laporan-TokoShiSop.tsv
+
+echo
+
+awk -F"\t"  '
+BEGIN {}
+{segment=$8
+    if (NR == 1) next 
+
+    count[segment]++
+} 
+END {min=99999;tipe=0;
+for (segment in count) {
+    if (min > count[segment]) {
+        min=count[segment]
+        tipe=segment
+    }
+}
+
+    printf("Tipe segmen customer yang penjualannya paling sedikit adalah %s dengan %s transaksi.", tipe, min)
 }' Laporan-TokoShiSop.tsv
