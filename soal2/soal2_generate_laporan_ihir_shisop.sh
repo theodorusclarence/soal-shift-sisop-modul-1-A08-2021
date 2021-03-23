f520893afbec440cc185ec5c894a15a2b54c3866
Theodorus Clarence <theodorusclarence@gmail.com>
2021-03-23 23:00:39 +0700

    2a finished

--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -2,3 +2,21 @@
 
 # cara separate tabs
 # awk -F$'\t' '{print $1}'
+
+
+awk -F"\t" '
+BEGIN {max=0;rid=0;}
+{sales=$18;profit=$21;rid=$2;
+	costprice=sales-profit;
+    if (costprice != 0)
+    {
+        profper=(profit/costprice)*100;
+    }
+    if (max <= profper) {
+        max=profper
+        maxidx=rid
+    }
+}
+END {
+    printf("Transaksi terakhir dengan profit percentage terbesar yaitu %s dengan persentase %s.\n", maxidx,max)
+}' Laporan-TokoShiSop.tsv
