cde67d7894dee6c6e3bac5e09521eb6b8b53b322
MuhammadNevin <nevin.muhammad@gmail.com>
2021-03-24 20:18:15 +0700

    push nevin #1

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# a) Mengumpulkan jenis log (ERROR/INFO), pesan log, dan username
+while IFS=$' ' read -r -a myArray
+do
+unset 'myArray[${#myArray[@]}-1]'
+# echo "${myArray[0]}, ${myArray[1]}, ${myArray[2]}, ${myArray[3]}, ${myArray[5]}"
+ echo "${myArray[@]:5}"
+# echo "${myArray[${#myArray[@]}-2]}"
+done < syslog.log
