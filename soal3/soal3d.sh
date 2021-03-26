c8e346f8350b14cfaa82c97501ef23d89a798900
Theodorus Clarence <theodorusclarence@gmail.com>
2021-03-26 20:38:21 +0700

    finished cron 3

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+cd soal-shift-sisop-modul-1-A08-2021/soal3
 
-zip -r -e -P $(date +%d%m%Y) Koleksi.zip /home/yusuf/soal-shift-sisop-modul-1-A08-2021/soal3
+jam=`date +%H`
+
+if [[ $jam -eq 7 ]]
+then
+    zip -r -e -P $(date +%m%d%Y) Koleksi.zip */
+    # remove all zip
+    rm -r */
+elif [[ $jam -eq 18 ]]
+then
+    unzip -P $(date +%m%d%Y) Koleksi.zip
+    rm Koleksi.zip
+fi
+
